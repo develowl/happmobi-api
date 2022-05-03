299c7159a5c9b846b9ca233e505332c2d9aef8e0
Yan Gonçalves <fgyan94@gmail.com>
2022-05-03 14:41:02 -0300

    :fire: Removing unused dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { UsersModule } from './features/users/users.module'
 import ormconfig from 'ormconfig'
 import { AuthModule } from './features/auth/auth.module'
-import { ConfigModule } from '@nestjs/config'
 import { CarsModule } from './features/cars/cars.module'
 import { RentalsModule } from './features/rentals/rentals.module'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(ormconfig),
-    AuthModule,
-    UsersModule,
-    CarsModule,
-    RentalsModule
-  ],
+  imports: [TypeOrmModule.forRoot(ormconfig), AuthModule, UsersModule, CarsModule, RentalsModule],
   controllers: [],
   providers: []
 })
